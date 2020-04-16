3162de2ac0acc934b948d9c57e4e545d6ccc6760
Jakub Glinka <glinka.jakub@outlook.com>
2020-04-16 13:41:40 +0200

    add Category class

---
+++ src/main/java/com/glinka/wcn/dto/Category.java
@@ -0,0 +1,11 @@
+package com.glinka.wcn.dto;
+
+import lombok.Data;
+
+@Data
+public class Category {
+
+    private int id;
+    private String name;
+
+}
--- src/main/java/com/glinka/wcn/dto/ScientificJournal.java
+++ src/main/java/com/glinka/wcn/dto/ScientificJournal.java
@@ -2,6 +2,8 @@ package com.glinka.wcn.dto;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class ScientificJournal {
 
@@ -13,49 +15,5 @@ public class ScientificJournal {
     private String issn2;
     private String eissn2;
     private int points;
-    private boolean n101;
-    private boolean n102;
-    private boolean n103;
-    private boolean n104;
-    private boolean n105;
-    private boolean n106;
-    private boolean n107;
-    private boolean n201;
-    private boolean n202;
-    private boolean n203;
-    private boolean n204;
-    private boolean n205;
-    private boolean n206;
-    private boolean n207;
-    private boolean n208;
-    private boolean n209;
-    private boolean n301;
-    private boolean n302;
-    private boolean n303;
-    private boolean n304;
-    private boolean n401;
-    private boolean n402;
-    private boolean n403;
-    private boolean n404;
-    private boolean n405;
-    private boolean n501;
-    private boolean n502;
-    private boolean n503;
-    private boolean n504;
-    private boolean n505;
-    private boolean n506;
-    private boolean n507;
-    private boolean n508;
-    private boolean n509;
-    private boolean n510;
-    private boolean n511;
-    private boolean n601;
-    private boolean n602;
-    private boolean n603;
-    private boolean n604;
-    private boolean n605;
-    private boolean n606;
-    private boolean n607;
-    private boolean n701;
-
+    private List<Category> categories;
 }
