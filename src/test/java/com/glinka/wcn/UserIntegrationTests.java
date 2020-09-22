ebab4038e70e019a20870d6517c3700c16a9cca3
Jakub Glinka <glinka.jakub@outlook.com>
2020-09-22 22:53:44 +0200

    add User tests

--- src/test/java/com/glinka/wcn/UserIntegrationTests.java
+++ src/test/java/com/glinka/wcn/UserIntegrationTests.java
@@ -1,9 +1,7 @@
 package com.glinka.wcn;
 
-import com.glinka.wcn.commons.ResourceNotFoundException;
 import com.glinka.wcn.model.dto.User;
 import com.glinka.wcn.service.UserService;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,8 +16,9 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD) //delete date from table
-class WcnApplicationTests {
+    //delete data from table
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+class UserIntegrationTests {
 
     @LocalServerPort
     private int port;
@@ -39,19 +38,27 @@ class WcnApplicationTests {
                 user,
                 User.class);
         assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(1, response.getBody().getId());
+        assertEquals("email@wp.pl", response.getBody().getEmail());
+        assertEquals("password", response.getBody().getPassword());
         assertEquals("name1", response.getBody().getName());
+        assertEquals("surname1", response.getBody().getSurname());
     }
 
     @Test
-    public void testGetUser() throws Exception {
+    public void testGetUsers() throws Exception {
         HttpEntity<String> entity = new HttpEntity<>(null, headers);
+        User user1 = new User(1, "email@wp.pl", "password", "name1", "surname1");
+        User user2 = new User(2, "email2@wp.pl", "password2", "name2", "surname2");
+        userService.save(user1);
+        userService.save(user2);
         ResponseEntity<User[]> response = restTemplate.exchange(
                 createURLWithPort("/api/user/allUsers"),
                 HttpMethod.GET,
                 entity,
                 User[].class);
         assertEquals(HttpStatus.OK, response.getStatusCode());
-        assertEquals(0, response.getBody().length);
+        assertEquals(2, response.getBody().length);
     }
 
     @Test
@@ -69,7 +76,11 @@ class WcnApplicationTests {
                 id);
         //then
         assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(1, response.getBody().getId());
+        assertEquals("email@wp.pl", response.getBody().getEmail());
+        assertEquals("password", response.getBody().getPassword());
         assertEquals("name1", response.getBody().getName());
+        assertEquals("surname1", response.getBody().getSurname());
     }
 
     @Test
@@ -87,7 +98,11 @@ class WcnApplicationTests {
                 name);
         //then
         assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(1, response.getBody()[0].getId());
+        assertEquals("email@wp.pl", response.getBody()[0].getEmail());
+        assertEquals("password", response.getBody()[0].getPassword());
         assertEquals("name1", response.getBody()[0].getName());
+        assertEquals("surname1", response.getBody()[0].getSurname());
     }
 
     @Test
@@ -105,7 +120,11 @@ class WcnApplicationTests {
                 email);
         //then
         assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(1, response.getBody().getId());
+        assertEquals("email@wp.pl", response.getBody().getEmail());
+        assertEquals("password", response.getBody().getPassword());
         assertEquals("name1", response.getBody().getName());
+        assertEquals("surname1", response.getBody().getSurname());
     }
 
     @Test
@@ -114,13 +133,20 @@ class WcnApplicationTests {
         HttpEntity<String> entity = new HttpEntity<>(null, headers);
         List<Integer> ids = new ArrayList<>();
         ids.add(1);
-        ids.add(2);
+        ids.add(3);
         String idsToString = ids.toString()
                 .replace("[", "")  //remove the right bracket
                 .replace("]", "")  //remove the left bracket
                 .trim();
-        userService.save(new User(1, "email@wp.pl", "password", "name1", "surname1"));
-        userService.save(new User(2, "email2@wp.pl", "password2", "name2", "surname2"));
+        User user1 = new User(1, "email@wp.pl", "password", "name1", "surname1");
+        User user2 = new User(2, "email2@wp.pl", "password2", "name2", "surname2");
+        User user3 = new User(3, "email3@wp.pl", "password2", "name2", "surname2");
+        User user4 = new User(4, "email4@wp.pl", "password2", "name2", "surname2");
+        userService.save(user1);
+        userService.save(user2);
+        userService.save(user3);
+        userService.save(user4);
+
         //when
         ResponseEntity<User[]> response = restTemplate.exchange(
                 createURLWithPort("/api/user/findAllUsersByIds?ids={idsToString}"),
@@ -130,12 +156,19 @@ class WcnApplicationTests {
                 idsToString);
         //then
         assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(2, response.getBody().length);
+        assertEquals(user1, response.getBody()[0]);
+//        assertEquals(user2, response.getBody()[1]);
+        assertEquals(1, response.getBody()[0].getId());
+        assertEquals("email@wp.pl", response.getBody()[0].getEmail());
+        assertEquals("password", response.getBody()[0].getPassword());
         assertEquals("name1", response.getBody()[0].getName());
+        assertEquals("surname1", response.getBody()[0].getSurname());
     }
 
     @Test
     void contextLoads() {
-        assertEquals(4, 2+2);
+        assertEquals(4, 2 + 2);
     }
 
     private String createURLWithPort(String uri) {
